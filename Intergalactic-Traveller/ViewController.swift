a10203bdfb1e3f87a22601f0c46fb39bd289b162
Victoria Mahoney <63237@psdschools.org>
2022-03-08 13:24:45 -0700

    Initial Commit

---
+++ Intergalactic-Traveller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Intergalactic-Traveller
+//
+//  Created by Victoria Mahoney on 3/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
