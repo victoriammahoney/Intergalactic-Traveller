5d1f99cc56ff570ab8647cfa74387132696a90ab
Victoria Mahoney <63237@psdschools.org>
2022-03-09 14:17:11 -0700

    progress

---
+++ Intergalactic-Traveller/StarViewController.swift
@@ -0,0 +1,32 @@
+//
+//  StarViewController.swift
+//  Intergalactic-Traveller
+//
+//  Created by Victoria Mahoney on 3/9/22.
+//
+
+import UIKit
+
+class StarViewController: UIViewController {
+
+    @IBOutlet weak var starImageView: UIImageView!
+    var starName : String = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
